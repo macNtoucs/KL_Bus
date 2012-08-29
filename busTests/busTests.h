c00bfeb8c6904138307249549b4d31ed47d3f37f
Rick <hackerick4@gmail.com>
2012-08-29 14:41:46 +0800

    初始化雜物

---
+++ busTests/busTests.h
@@ -0,0 +1,13 @@
+//
+//  busTests.h
+//  busTests
+//
+//  Created by mac_hero on 12/5/4.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface busTests : SenTestCase
+
+@end
---
+++ busTests/busTests.m
@@ -0,0 +1,32 @@
+//
+//  busTests.m
+//  busTests
+//
+//  Created by mac_hero on 12/5/4.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "busTests.h"
+
+@implementation busTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+
+}
+
+@end
